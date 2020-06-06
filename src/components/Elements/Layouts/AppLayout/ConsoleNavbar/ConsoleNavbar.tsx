ec202eb99a986d75e5662b5f8a8aa40f5d35b6a9
rosborne132 <ozborne132@gmail.com>
2020-06-06 14:00:30 -0700

    Update layout

--- src/components/Elements/Layouts/AppLayout/ConsoleNavbar/ConsoleNavbar.tsx
+++ src/components/Elements/Layouts/AppLayout/ConsoleNavbar/ConsoleNavbar.tsx
@@ -50,66 +50,68 @@ export const ConsoleNavbar: React.FC<ConsoleNavbarProps> = React.memo(({ console
 
     return (
         <AnimatePresence>
-            <div className="fixed flex  top-25 vh-50  w5 z-3">
-                {isShowing && (
-                    <motion.ul
-                        className="h-100 list ml0 overflow-auto pl0 relative w4-5"
-                        variants={variants}
-                        initial="closed"
-                        animate="open"
-                        exit="closed"
-                        data-testid="consoleNavbar"
-                    >
-                        {navbarButtons.map(({ component, type }: { component: any; type: string }) => {
-                            if (type === 'game' && !consoles.length) return false
-                            return (
-                                <li
-                                    key={type}
-                                    className={`${linkStyle} ${showTab === type ? 'bg-black white' : 'bg-white'}`}
-                                    onClick={() => {
-                                        setShowTab(type)
-                                        openModal(component())
-                                    }}
-                                >
-                                    <span>
-                                        <Icon
-                                            icon="add"
-                                            size="m1"
-                                            style={{
-                                                fill: showTab === type ? 'white' : 'black'
-                                            }}
-                                            aria-hidden
-                                        />
-                                        {` ${upperFirst(type)}`}
-                                    </span>
-                                </li>
-                            )
-                        })}
-
-                        <li
-                            className={`${linkStyle} ${selected === '' ? 'bg-black white' : 'bg-white'}`}
-                            onClick={() => router.push('/app')}
+            <div className="fixed top-25">
+                <div className="flex w5 z-3" style={{ minHeight: '100%', maxHeight: '50vh' }}>
+                    {isShowing && (
+                        <motion.ul
+                            className="list ml0 overflow-auto pl0 relative w4-5"
+                            variants={variants}
+                            initial="closed"
+                            animate="open"
+                            exit="closed"
+                            data-testid="consoleNavbar"
                         >
-                            Backlog
-                        </li>
-                        {consoles.map(({ console: { id, name } }: { console: { id: string; name: string } }) => (
+                            {navbarButtons.map(({ component, type }: { component: any; type: string }) => {
+                                if (type === 'game' && !consoles.length) return false
+                                return (
+                                    <li
+                                        key={type}
+                                        className={`${linkStyle} ${showTab === type ? 'bg-black white' : 'bg-white'}`}
+                                        onClick={() => {
+                                            setShowTab(type)
+                                            openModal(component())
+                                        }}
+                                    >
+                                        <span>
+                                            <Icon
+                                                icon="add"
+                                                size="m1"
+                                                style={{
+                                                    fill: showTab === type ? 'white' : 'black'
+                                                }}
+                                                aria-hidden
+                                            />
+                                            {` ${upperFirst(type)}`}
+                                        </span>
+                                    </li>
+                                )
+                            })}
+
                             <li
-                                key={id}
-                                className={`${linkStyle} ${selected === id ? 'bg-black white' : 'bg-white'}`}
-                                onClick={() => router.push('/app/[id]', `/app/${id}`)}
+                                className={`${linkStyle} ${selected === '' ? 'bg-black white' : 'bg-white'}`}
+                                onClick={() => router.push('/app')}
                             >
-                                {name}
+                                Backlog
                             </li>
-                        ))}
-                    </motion.ul>
-                )}
-                <button
-                    onClick={() => setIsShowing(!isShowing)}
-                    style={{ background: 'none' }}
-                    className="bn h-100 pointer"
-                >
-                    <div className="bg-white br4 pa2 pointer">{isShowing ? '<' : '>'}</div>
-                </button>
+                            {consoles.map(({ console: { id, name } }: { console: { id: string; name: string } }) => (
+                                <li
+                                    key={id}
+                                    className={`${linkStyle} ${selected === id ? 'bg-black white' : 'bg-white'}`}
+                                    onClick={() => router.push('/app/[id]', `/app/${id}`)}
+                                >
+                                    {name}
+                                </li>
+                            ))}
+                        </motion.ul>
+                    )}
+                    <button
+                        onClick={() => setIsShowing(!isShowing)}
+                        style={{ background: 'none' }}
+                        className="bn pointer"
+                    >
+                        <div className="bg-white br4 pa2 flex align-center pointer">{isShowing ? '<' : '>'}</div>
+                    </button>
+                </div>
             </div>
         </AnimatePresence>
     )
