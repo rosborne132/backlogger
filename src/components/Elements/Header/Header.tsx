b188853b78cbc0c5218d95195a4001f61a1d9a8a
Rob Osborne <robosborne@pop-os.localdomain>
2020-07-01 22:11:37 -0700

    Update header

--- src/components/Elements/Header/Header.tsx
+++ src/components/Elements/Header/Header.tsx
@@ -3,47 +3,21 @@ import { useRouter } from 'next/router'
 
 import { useAuthFunctions } from 'src/lib/auth'
 
-const HeaderLink = ({
-    className,
-    children,
-    onClick
-}: {
-    className?: string
-    children: React.ReactNode
-    onClick?: any
-}) => (
-    <li className={`dib pointer no-underline pointer white ${className}`} onClick={onClick}>
-        {children}
-    </li>
-)
-
-// const Header = ({ children }: { children: React.ReactNode }) => (
-//     <header data-testid="header" className="bg-black pa1">
-//         <nav className="container">
-//             <ul className="list flex justify-between pa0 sans-serif">{children}</ul>
-//         </nav>
-//     </header>
-// )
-
-const Header = ({ children }: { children: React.ReactNode }) => {
-    const router = useRouter()
-    return (
-        <header data-testid="header">
-            <h1 className="fake-logo">
-                <a onClick={() => router.push({ pathname: `/app` })}>Backlogger</a>
-            </h1>
-            <nav>{children}</nav>
-        </header>
-    )
-}
+const Header = ({ children }: { children: React.ReactNode }) => <header data-testid="header">{children}</header>
 
 export const LandingHeader: React.FC = React.memo(
     (): JSX.Element => {
+        const router = useRouter()
         const { login } = useAuthFunctions()
 
         return (
             <Header>
-                <a onClick={() => login()}>Login</a>
+                <h1 className="fake-logo">
+                    <a onClick={() => router.push({ pathname: `/` })}>Backlogger</a>
+                </h1>
+                <nav>
+                    <a onClick={() => login()}>Login</a>
+                </nav>
             </Header>
         )
     }
@@ -51,11 +25,17 @@ export const LandingHeader: React.FC = React.memo(
 
 export const AppHeader: React.FC = React.memo(
     (): JSX.Element => {
+        const router = useRouter()
         const { logout } = useAuthFunctions()
 
         return (
             <Header>
-                <a onClick={() => logout()}>Logout</a>
+                <h1 className="fake-logo">
+                    <a onClick={() => router.push({ pathname: `/app` })}>Backlogger</a>
+                </h1>
+                <nav>
+                    <a onClick={() => logout()}>Logout</a>
+                </nav>
             </Header>
         )
     }
